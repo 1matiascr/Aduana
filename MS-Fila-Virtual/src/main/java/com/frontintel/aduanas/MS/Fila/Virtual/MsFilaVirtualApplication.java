59e0bbfec26919d74c83ab41f81341ca2ba3eebf
javortiz2018-bit <javortiz2018@gmail.com>
2026-06-10 11:30:14 -0400

    Add files via upload

---
+++ MS-Fila-Virtual/src/main/java/com/frontintel/aduanas/MS/Fila/Virtual/MsFilaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.frontintel.aduanas.MS.Fila.Virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFilaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFilaVirtualApplication.class, args);
+	}
+
+}
